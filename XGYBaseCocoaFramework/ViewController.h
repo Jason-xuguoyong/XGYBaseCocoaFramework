d02fc98f6e844bee8e1965b8b040233d1cf68a4d
Jason <Jason@laowo.me>
2017-02-23 16:47:16 +0800

    Initial Commit

---
+++ XGYBaseCocoaFramework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XGYBaseCocoaFramework
+//
+//  Created by Jason on 2017/2/23.
+//  Copyright © 2017年 Jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
